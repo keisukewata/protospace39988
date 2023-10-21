aebc084cd8781346e1583593f4e597101f74da26
keisukewata <spclzd6667@gmail.com>
2023-10-22 05:26:24 +0900

    first commit

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,7 @@
+class UserController < ApplicationController
+  def show
+    user_id = params[:id]
+    @user = User.find_by(user_id)
+    @prototype = @user.prototype
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get 'prototypes/index'
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root to: "prototypes#index"
+  resources :prototypes do
+    resources :comments, only: :create
+  end
+  
+resources :user, only: :show
+
+end
