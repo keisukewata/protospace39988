44bfd661af4a79d12dc07035c46dc56d12f2c0a4
keisukewata <spclzd6667@gmail.com>
2023-10-23 17:24:35 +0900

    四度目の正直

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,11 +1,11 @@
 class UserController < ApplicationController
   def show
-    user_id = params[:id]
-    @user = User.find_by(user_id)
+   
+    @user = User.find(params[:id])
     @prototype = @user.prototype
   end
 
   def create
   end
-  
+
 end
